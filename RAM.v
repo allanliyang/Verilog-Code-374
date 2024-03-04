aae3196d6bafe50f75fce0f6acf8ba7a99aa3ad2
SalmaIsmail7 <157075822+SalmaIsmail7@users.noreply.github.com>
2024-03-04 14:00:47 -0500

    Update RAM.v

--- RAM.v
+++ RAM.v
@@ -1,22 +1,29 @@
-module RAM(Cs_b, We_b, Oe_b, Addrress, IO); 
-	input Cs_b;					//chip select 
-	input We_b;					//write enable/ read enable 
-	input Oe_b;					// output enable; 
-	input[8:0] Address; 		// address input 
-	inout[31:0] IO;
+module RAM(We_b, Re_b, Addrress, IO); 
+	input We_b;					//write enable
+	input Re_b;					//Read enable; 
+	input[8:0] Address; 				// address input 
+	input[31:0]BusMuxIn; 
+	output[31:0]MDRMux; 
+
 	
-	reg[8:0] mem[0:511];  //2^9 
+	reg[8:0] mem[0:511];  // RAM, 2^9 
+	reg[8:0] data; 
 	
-	assign IO = ((Cs_b == 1'b1 | We_b == 1'b1 | Oe_b == 1'b1) ? 
-		9'bZ: mem[Address]; //read from RAM
-	always @(We_b, Cs_b) 
-	begin 
-		@(negedge We_b) // falling edge of We_b 
-		if(Cs_b == 1'b0)
-		begin
-		mem[Address] <= I0;  //write to RAM
+	
+	//Read to RAM When We = 0 and Re = 1 
+	always @(Address or We_b or Re_b) 
+		begin 
+			if(Re_b && !We_b)
+				data <= mem[Address]; 
 		end 
-	end 
+	// Write to RAM	
+	always @( Address or IO or We_b) 
+		begin 
+			if(We_b == 1'b1)
+			mem[Address] <= IO; 
+		end 
+	assign MDRMux = data; 
 	endmodule 
 		
 		
+
