7f774a57ba2aba4eed70aaf693942690645a16c3
allanliyang <20aly2@queensu.ca>
2024-03-04 22:43:36 -0500

    Added initial LDI testbench

---
+++ datapath_tb_LDI.v
@@ -0,0 +1,18 @@
+// T0: PCout, MARin, IncPC, Zin
+// T1: Zlowout, PCin, MDMuxread, Mdatain, MDRin
+// T2: MDRout, IRin
+// T3: Grb, BAout, Yin
+// T4: Cout, ADD, Zin
+// T5: Zlowout, Gra, Rin
+
+// testbench for LD instruction
+// NOTE: FIX TESTBENCH SETTINGS BEFORE RUNNING THIS
+
+// functionality
+	// this tb performs ldi R2,0x95 and ldi R0, 0x38(R2)
+	
+		
+`timescale 1ns/10ps
+module datapath_tb_LDI();
+
+endmodule
