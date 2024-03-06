cb29b06a43e421630ed602b173d4526170d55bb4
Allan <96800916+allanliyang@users.noreply.github.com>
2024-03-06 17:55:10 -0500

    Update RAM.v

--- RAM.v
+++ RAM.v
@@ -1,16 +1,21 @@
 module RAM(
 	input read, write,
 	input [8:0]address,
-	input [31:0]BusMuxOut, 
-	output[31:0]Mdatain
+	input [31:0]BusMuxOut,
+	output wire [31:0]Mdatain
 	);
 
 	
-	reg[31:0] mem[0:511];  // 36x512 RAM
-	reg[31:0] data;
+	reg [31:0]mem[0:511];  // 36x512 RAM
+	reg [31:0]data;
+
+	initial begin
+		//$readmemh(/*PATH*/, mem, 0, 511); // WARNING: UPDATE PATH BEFORE RUNNING
+	end
 	
 	// output from ram when read == 1 and write == 0
-	always @ (address, write, read) begin 
+	always @ (address, read, write) begin 
+			
 			if(read && !write) begin
 				data <= mem[address];
 			end
@@ -21,8 +26,11 @@ module RAM(
 	
 	// write to ram when write == 1
 	always @ (address, BusMuxOut, write) begin 
+			
 			if (write) begin 
-				mem[address] <= BusMuxOut; 
+				mem[address] <= BusMuxOut;
+				// update text file to reflect ram contents
+				//$writememh(/*PATH*/, mem, address, address); // WARNING: UPDATE PATH BEFORE RUNNING
 			end
 			
 	end
