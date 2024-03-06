6d98f414f00c14f66c4bea17c4a3ec39e5a244e8
Allan <96800916+allanliyang@users.noreply.github.com>
2024-03-05 21:23:14 -0500

    Create datapath_tb_IN.v

---
+++ datapath_tb_IN.v
@@ -0,0 +1,17 @@
+// T0: PCout, MARin, IncPC, Zin
+// T1: Zlowout, PCin, MDMuxread, Mdatain, MDRin
+// T2: MDRout, IRin
+// T3: InPortOut, Gra, Rin
+
+// testbench for LD instruction
+// NOTE: FIX TESTBENCH SETTINGS BEFORE RUNNING THIS
+
+// functionality
+	// this tb performs 'in R4'	
+		
+		
+		
+`timescale 1ns/10ps
+module datapath_tb_IN();
+
+endmodule
