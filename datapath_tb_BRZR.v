f8e15d9aefe508575a5b89528f3afc147c8dc4fc
Allan <96800916+allanliyang@users.noreply.github.com>
2024-03-07 00:45:10 -0500

    Update and rename datapath_tb_BR.v to datapath_tb_BRZR.v

---
+++ datapath_tb_BRZR.v
@@ -0,0 +1,15 @@
+// T0: PCout, MARin, IncPC, Zin
+// T1: Zlowout, PCin, MDMuxread, Mdatain, MDRin
+// T2: MDRout, IRin
+// T3: Gra, Rout, CONin
+// T4: PCout, Yin
+// T5: Cout, ADD, Zlowin
+// T6: Zlowout, if(CONFF) then CON -> PCin
+
+// functionality
+  // this tb performs brzr R5, 14 (branch if zero)
+
+
+
+`timescale 1ns/10ps
+module datapath_tb_BR();
